433a4a0cbe508f482431d1e49bfd036a93e2c6d2
Илья Грачев <grachev.ia96@gmail.com>
2022-10-31 13:18:59 +0700

    added

---
+++ Conditional/src/main/java/ru/netology/conditional/JavaConfig/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.conditional.JavaConfig;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditional.systemProfile.DevProfile;
+import ru.netology.conditional.systemProfile.ProductionProfile;
+import ru.netology.conditional.systemProfile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "netology.profile", name = "dev", havingValue = "false")
+    public SystemProfile ProdProfile() {
+        return new ProductionProfile();
+    }
+}
